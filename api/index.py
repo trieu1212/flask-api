6476f7ae094d1170ffac3fcd3a297c71c22cc5cb
trieu1212 <lequoctrieu618@gmail.com>
2024-11-30 21:29:04 +0700

    flask-api

--- api/index.py
+++ api/index.py
@@ -12,5 +12,5 @@ def test():
     return jsonify({'message': 'Test endpoint is working!'}), 200
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
 
